6270783f2ad5ea2b14c8fa197339a15ca635e138
Rik de Wijn <rdwijn@pamgene.com>
2024-11-25 12:34:57 +0000

    test2

--- main.R
+++ main.R
@@ -89,7 +89,7 @@ pdp = pdp +
   facet_grid(.~panels, scales = "free_x", space = "free_x") 
 #vp = viewport(width = unit(pheight, "in"), height = unit(stripwidth(df), "in"))
 #p = print(pdp, vp = vp)
-plot_file <- tim::save_plot(pdp, width = pheight, height = stripwidth(df), dpi = 300)
+plot_file <- tim::save_plot(pdp, width = pheight, height = 2*stripwidth(df), dpi = 300, units  = "in")
 
 df_plot <- tim::plot_file_to_df(plot_file) %>%
   mutate(.ci = 0L, .ri = 0L) %>%
